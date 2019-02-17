67815ee05e028525359ae69fe18a4143b1d1eb6e
simonwinkler101 <47678783+simonwinkler101@users.noreply.github.com>
2019-02-17 13:24:56 +1100

    scripts and a whole lot more

---
+++ resources/js/scripts.js
@@ -0,0 +1,15 @@
+cssVars();
+
+// mobile nav
+
+$('.nav-prompt').click(function () {
+
+if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+    $('.nav-inner').attr('aria-expanded', 'true');
+} else {
+    $('.nav-inner').attr('aria-expanded', 'false');
+}
+
+
+});
